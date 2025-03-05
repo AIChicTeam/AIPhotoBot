bd540e21bd6920fbac1a74ae0ebceb3a9de9cb87
trekfest <hrynyshyn-ee27@lll.kpi.ua>
2025-03-05 14:59:43 +0200

    Fix

--- ai_photo_bot/urls.py
+++ ai_photo_bot/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("bot/", include("bot_api.urls")),
     path("payments/", include("payments.urls")),
-    path('', include('django_prometheus.urls')),
 ]
 
 # Serve media files in development mode
